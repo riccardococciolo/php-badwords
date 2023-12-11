f023dd1a0160867ea57ed8213e54fc94670505d1
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2023-12-11 14:30:33 +0100

    add: form

--- index.php
+++ index.php
@@ -10,6 +10,16 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Dimmi una frase e la parola da eliminare</h1>
+
+        <form action="badwords.php" method="GET">
+        <label for="text">Frase</label>
+        <input type="text" id="text" name="text">
+
+        <label for="badwords">Parola da eliminare</label>
+        <input type="text" id="badwords" name="badwords">
+
+        <button type="submit">Invia</button>
+    </form>
 </body>
 </html>
